b042dd98d08be59380726a48ab11227d64c5fca4
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-22 16:22:40 +0100

    ⚡️ Add support for `save_file` and `load_file`

--- src/safejax/flax.py
+++ src/safejax/flax.py
@@ -3,7 +3,9 @@ from typing import Any, Dict, Union
 import numpy as np
 from flax.core.frozen_dict import FrozenDict, freeze
 from jax import numpy as jnp
-from safetensors.flax import load, save
+from safetensors.flax import load, load_file, save, save_file
+
+from safejax.typing import PathLike
 
 
 def flatten_frozen_dict(
@@ -24,11 +26,19 @@ def flatten_frozen_dict(
     return weights
 
 
-def serialize(frozen_or_unfrozen_dict: Union[Dict[str, Any], FrozenDict]) -> bytes:
+def serialize(
+    frozen_or_unfrozen_dict: Union[Dict[str, Any], FrozenDict],
+    filename: Union[PathLike, None] = None,
+) -> bytes:
+    # TODO(alvaro): handle the errors properly
     flattened_dict = flatten_frozen_dict(
         frozen_or_unfrozen_dict=frozen_or_unfrozen_dict
     )
-    return save(tensors=flattened_dict)
+    return (
+        save(tensors=flattened_dict)
+        if not filename
+        else save_file(tensors=flattened_dict, filename=filename)
+    )
 
 
 def unflatten_frozen_dict(tensors: Dict[str, jnp.DeviceArray]) -> FrozenDict:
@@ -42,6 +52,10 @@ def unflatten_frozen_dict(tensors: Dict[str, jnp.DeviceArray]) -> FrozenDict:
     return freeze(weights)
 
 
-def deserialize(data: bytes) -> FrozenDict:
-    loaded_dict = load(data=data)
+def deserialize(path_or_buf: Union[PathLike, bytes]) -> FrozenDict:
+    # TODO(alvaro): handle the errors properly
+    if isinstance(path_or_buf, bytes):
+        loaded_dict = load(data=path_or_buf)
+    if isinstance(path_or_buf, PathLike):
+        loaded_dict = load_file(filename=path_or_buf)
     return unflatten_frozen_dict(tensors=loaded_dict)
