4c7dc074716a95e799a2ed71e504961acf57e097
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-23 21:26:36 +0100

    💡 Add `safejax.flax` docstrings

--- src/safejax/flax.py
+++ src/safejax/flax.py
@@ -16,7 +16,23 @@ def flatten_dict(
     frozen_or_unfrozen_dict: Union[Dict[str, Any], FrozenDict],
     key_prefix: Union[str, None] = None,
 ) -> Union[Dict[str, jnp.DeviceArray], Dict[str, np.ndarray]]:
-    """Idea from https://gist.github.com/Narsil/d5b0d747e5c8c299eb6d82709e480e3d"""
+    """
+    Flatten a `FrozenDict` or a `Dict` containing Flax model parameters.
+
+    Note:
+        This function is recursive to explore all the nested dictionaries,
+        and the keys are being flattened using the `.` character. So that the
+        later de-nesting can be done using the `.` character as a separator.
+
+    Reference at https://gist.github.com/Narsil/d5b0d747e5c8c299eb6d82709e480e3d
+
+    Args:
+        frozen_or_unfrozen_dict: A `FrozenDict` or a `Dict` containing the model parameters.
+        key_prefix: A prefix to prepend to the keys of the flattened dictionary.
+
+    Returns:
+        A flattened dictionary containing the model parameters.
+    """
     weights = {}
     for key, value in frozen_or_unfrozen_dict.items():
         key = f"{key_prefix}.{key}" if key_prefix else key
@@ -24,9 +40,7 @@ def flatten_dict(
             weights[key] = value
             continue
         if isinstance(value, FrozenDict) or isinstance(value, Dict):
-            weights.update(
-                flatten_frozen_dict(frozen_or_unfrozen_dict=value, key_prefix=key)
-            )
+            weights.update(flatten_dict(frozen_or_unfrozen_dict=value, key_prefix=key))
     return weights
 
 
@@ -34,10 +48,20 @@ def serialize(
     frozen_or_unfrozen_dict: Union[Dict[str, Any], FrozenDict],
     filename: Union[PathLike, None] = None,
 ) -> Union[bytes, PathLike]:
-    # TODO(alvaro): handle the errors properly
-    flattened_dict = flatten_frozen_dict(
-        frozen_or_unfrozen_dict=frozen_or_unfrozen_dict
-    )
+    """
+    Serialize a Flax model from either a `FrozenDict` or a `Dict`.
+
+    If `filename` is not provided, the serialized model is returned as a `bytes` object,
+    otherwise the model is saved to the provided `filename` and the `filename` is returned.
+
+    Args:
+        frozen_or_unfrozen_dict: A `FrozenDict` or a `Dict` containing the model parameters.
+        filename: The path to the file where the model will be saved.
+
+    Returns:
+        The serialized model as a `bytes` object or the path to the file where the model was saved.
+    """
+    flattened_dict = flatten_dict(frozen_or_unfrozen_dict=frozen_or_unfrozen_dict)
     if not filename:
         return save(tensors=flattened_dict)
     else:
@@ -46,6 +70,17 @@ def serialize(
 
 
 def unflatten_dict(tensors: Dict[str, jnp.DeviceArray]) -> FrozenDict:
+    """
+    Unflatten a `FrozenDict` from a `Dict` of tensors.
+
+    Reference at https://stackoverflow.com/a/63545677.
+
+    Args:
+        tensors: A `Dict` of tensors containing the model parameters.
+
+    Returns:
+        A `FrozenDict` containing the model parameters.
+    """
     weights = {}
     for key, value in tensors.items():
         subkeys = key.split(".")
@@ -56,7 +91,15 @@ def unflatten_dict(tensors: Dict[str, jnp.DeviceArray]) -> FrozenDict:
 
 
 def deserialize(path_or_buf: Union[PathLike, bytes]) -> FrozenDict:
-    # TODO(alvaro): handle the errors properly
+    """
+    Deserialize a Flax model from either a `bytes` object or a file path.
+
+    Args:
+        path_or_buf: A `bytes` object or a file path containing the serialized model.
+
+    Returns:
+        A `FrozenDict` containing the model parameters.
+    """
     if isinstance(path_or_buf, bytes):
         loaded_dict = load(data=path_or_buf)
     if (
@@ -65,4 +108,4 @@ def deserialize(path_or_buf: Union[PathLike, bytes]) -> FrozenDict:
         or isinstance(path_or_buf, os.PathLike)
     ):
         loaded_dict = load_file(filename=path_or_buf)
-    return unflatten_frozen_dict(tensors=loaded_dict)
+    return unflatten_dict(tensors=loaded_dict)
