941d26b284df591893b8d1c16718085f792285e5
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-24 14:25:32 +0100

    🧪 Extend unit tests to run over `ResNet50`
    `Resnet50` has been imported from `flaxmodels` and the weights are ported in `flaxmodels` from https://github.com/pytorch/vision

--- tests/test_flax.py
+++ tests/test_flax.py
@@ -6,39 +6,57 @@ from flax.core.frozen_dict import FrozenDict
 from safejax.flax import deserialize, serialize
 
 
-@pytest.mark.usefixtures("single_layer_model_params")
-def test_serialize(single_layer_model_params: FrozenDict) -> None:
-    serialized = serialize(params=single_layer_model_params)
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.lazy_fixture("single_layer_params"),
+        pytest.lazy_fixture("resnet50_params"),
+    ],
+)
+def test_serialize(params: FrozenDict) -> None:
+    serialized = serialize(params=params)
     assert isinstance(serialized, bytes)
     assert len(serialized) > 0
 
 
-@pytest.mark.usefixtures("single_layer_model_params", "safetensors_file")
-def test_serialize_to_file(
-    single_layer_model_params: FrozenDict, safetensors_file: Path
-) -> None:
-    safetensors_file = serialize(
-        params=single_layer_model_params, filename=safetensors_file
-    )
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.lazy_fixture("single_layer_params"),
+        pytest.lazy_fixture("resnet50_params"),
+    ],
+)
+@pytest.mark.usefixtures("safetensors_file")
+def test_serialize_to_file(params: FrozenDict, safetensors_file: Path) -> None:
+    safetensors_file = serialize(params=params, filename=safetensors_file)
     assert isinstance(safetensors_file, Path)
     assert safetensors_file.exists()
 
 
-@pytest.mark.usefixtures("single_layer_model_params")
-def test_deserialize(single_layer_model_params: FrozenDict) -> None:
-    serialized = serialize(params=single_layer_model_params)
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.lazy_fixture("single_layer_params"),
+        pytest.lazy_fixture("resnet50_params"),
+    ],
+)
+def test_deserialize(params: FrozenDict) -> None:
+    serialized = serialize(params=params)
     deserialized = deserialize(path_or_buf=serialized)
     assert isinstance(deserialized, FrozenDict)
     assert len(deserialized) > 0
 
 
-@pytest.mark.usefixtures("single_layer_model_params", "safetensors_file")
-def test_deserialize_from_file(
-    single_layer_model_params: FrozenDict, safetensors_file: Path
-) -> None:
-    safetensors_file = serialize(
-        params=single_layer_model_params, filename=safetensors_file
-    )
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.lazy_fixture("single_layer_params"),
+        pytest.lazy_fixture("resnet50_params"),
+    ],
+)
+@pytest.mark.usefixtures("safetensors_file")
+def test_deserialize_from_file(params: FrozenDict, safetensors_file: Path) -> None:
+    safetensors_file = serialize(params=params, filename=safetensors_file)
     deserialized = deserialize(path_or_buf=safetensors_file)
     assert isinstance(deserialized, FrozenDict)
     assert len(deserialized) > 0
