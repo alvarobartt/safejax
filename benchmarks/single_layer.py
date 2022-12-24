70c3f74843781d03820b46f9e0f2c02309fc9465
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-24 16:37:19 +0100

    ✨ Add `benchmarks/` for SingleLayer and ResNet50
    Split between scripts intended to be run with Python and the ones intended to be run with `hyperfine`

--- benchmarks/single_layer.py
+++ benchmarks/single_layer.py
@@ -1,9 +1,9 @@
-import sys
+from time import perf_counter
 
 import jax
-import jax.numpy as jnp
 from flax import linen as nn
 from flax.serialization import to_bytes
+from jax import numpy as jnp
 
 from safejax.flax import serialize
 
@@ -23,13 +23,14 @@ rng = jax.random.PRNGKey(0)
 params = model.init(rng, jnp.ones((1, 1)))
 
 
-def benchmark_safejax():
-    _ = serialize(params)
+start_time = perf_counter()
+for _ in range(100):
+    serialize(params)
+end_time = perf_counter()
+print(f"safejax (100 runs): {end_time - start_time:0.4f} s")
 
-
-def benchmark_flax():
-    _ = to_bytes(params)
-
-
-if __name__ == "__main__":
-    globals()[sys.argv[1]]()
+start_time = perf_counter()
+for _ in range(100):
+    to_bytes(params)
+end_time = perf_counter()
+print(f"flax (100 runs): {end_time - start_time:0.4f} s")
