e2e8f3f76fe0376edf1d3cf2cdc1ac01a80f25dc
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-26 11:01:16 +0100

    🔖 Set 0.2.0 version
    Already tested with both `flax` and `haiku` models out of the box, so `safejax` is out of `0.2.0.dev0` :)

--- src/safejax/__init__.py
+++ src/safejax/__init__.py
@@ -1,7 +1,7 @@
 """`safejax `: Serialize JAX, Flax, or Haiku model params with `safetensors`"""
 
 __author__ = "Alvaro Bartolome <alvarobartt@yahoo.com>"
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0"
 
 from safejax.load import deserialize  # noqa: F401
 from safejax.save import serialize  # noqa: F401
