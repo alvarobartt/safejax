a7f1bce3e8f6c355e0fcd942e7068bed8071e7c3
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-29 15:23:50 +0100

    ⚗️ Update unit tests for `safejax.core`

--- tests/test_core_load.py
+++ tests/test_core_load.py
@@ -1,35 +1,80 @@
 from pathlib import Path
+from typing import Any, Dict, Union
 
 import pytest
 from flax.core.frozen_dict import FrozenDict
+from objax.variable import VarCollection
 
-from safejax import deserialize, serialize
+from safejax.core.load import deserialize
+from safejax.core.save import serialize
+from safejax.typing import ParamsDictLike
 
 
 @pytest.mark.parametrize(
-    "params",
+    "params, deserialize_kwargs, expected_output_type",
     [
-        pytest.lazy_fixture("single_layer_params"),
-        pytest.lazy_fixture("flax_resnet50_params"),
+        (
+            pytest.lazy_fixture("flax_resnet50_params"),
+            {"freeze_dict": True},
+            FrozenDict,
+        ),
+        (pytest.lazy_fixture("flax_resnet50_params"), {"freeze_dict": False}, dict),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": True},
+            VarCollection,
+        ),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": False},
+            dict,
+        ),
+        (pytest.lazy_fixture("haiku_resnet50_params"), {}, dict),
     ],
 )
-def test_deserialize(params: FrozenDict) -> None:
+def test_deserialize(
+    params: ParamsDictLike,
+    deserialize_kwargs: Dict[str, Any],
+    expected_output_type: Union[dict, FrozenDict, VarCollection],
+) -> None:
     encoded_params = serialize(params=params)
-    decoded_params = deserialize(path_or_buf=encoded_params, freeze_dict=True)
-    assert isinstance(decoded_params, FrozenDict)
+    decoded_params = deserialize(path_or_buf=encoded_params, **deserialize_kwargs)
+    assert isinstance(decoded_params, expected_output_type)
     assert len(decoded_params) > 0
+    assert decoded_params.keys() == params.keys()
 
 
 @pytest.mark.parametrize(
-    "params",
+    "params, deserialize_kwargs, expected_output_type",
     [
-        pytest.lazy_fixture("single_layer_params"),
-        pytest.lazy_fixture("flax_resnet50_params"),
+        (
+            pytest.lazy_fixture("flax_resnet50_params"),
+            {"freeze_dict": True},
+            FrozenDict,
+        ),
+        (pytest.lazy_fixture("flax_resnet50_params"), {"freeze_dict": False}, dict),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": True},
+            VarCollection,
+        ),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": False},
+            dict,
+        ),
+        (pytest.lazy_fixture("haiku_resnet50_params"), {}, dict),
     ],
 )
 @pytest.mark.usefixtures("safetensors_file")
-def test_deserialize_from_file(params: FrozenDict, safetensors_file: Path) -> None:
+def test_deserialize_from_file(
+    params: FrozenDict,
+    deserialize_kwargs: Dict[str, Any],
+    expected_output_type: Union[dict, FrozenDict, VarCollection],
+    safetensors_file: Path,
+) -> None:
     safetensors_file = serialize(params=params, filename=safetensors_file)
-    decoded_params = deserialize(path_or_buf=safetensors_file, freeze_dict=True)
-    assert isinstance(decoded_params, FrozenDict)
+    decoded_params = deserialize(path_or_buf=safetensors_file, **deserialize_kwargs)
+    assert isinstance(decoded_params, expected_output_type)
     assert len(decoded_params) > 0
+    assert decoded_params.keys() == params.keys()
--- tests/test_core_save.py
+++ tests/test_core_save.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 import pytest
-from flax.core.frozen_dict import FrozenDict
 
-from safejax import serialize
+from safejax.core.save import serialize
+from safejax.utils import ParamsDictLike
 
 
 @pytest.mark.parametrize(
@@ -11,9 +11,11 @@ from safejax import serialize
     [
         pytest.lazy_fixture("single_layer_params"),
         pytest.lazy_fixture("flax_resnet50_params"),
+        pytest.lazy_fixture("objax_resnet50_params"),
+        pytest.lazy_fixture("haiku_resnet50_params"),
     ],
 )
-def test_serialize(params: FrozenDict) -> None:
+def test_serialize(params: ParamsDictLike) -> None:
     encoded_params = serialize(params=params)
     assert isinstance(encoded_params, bytes)
     assert len(encoded_params) > 0
@@ -24,10 +26,12 @@ def test_serialize(params: FrozenDict) -> None:
     [
         pytest.lazy_fixture("single_layer_params"),
         pytest.lazy_fixture("flax_resnet50_params"),
+        pytest.lazy_fixture("objax_resnet50_params"),
+        pytest.lazy_fixture("haiku_resnet50_params"),
     ],
 )
 @pytest.mark.usefixtures("safetensors_file")
-def test_serialize_to_file(params: FrozenDict, safetensors_file: Path) -> None:
+def test_serialize_to_file(params: ParamsDictLike, safetensors_file: Path) -> None:
     safetensors_file = serialize(params=params, filename=safetensors_file)
     assert isinstance(safetensors_file, Path)
     assert safetensors_file.exists()
