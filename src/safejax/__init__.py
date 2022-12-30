06f5f9759bb203ea1d5c1dd91e412e7c4d85a69f
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-30 17:02:46 +0100

    🔖 Set 0.4.0 version

--- src/safejax/__init__.py
+++ src/safejax/__init__.py
@@ -1,7 +1,7 @@
 """`safejax `: Serialize JAX, Flax, Haiku, or Objax model params with `safetensors`"""
 
 __author__ = "Alvaro Bartolome <alvarobartt@yahoo.com>"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from safejax.core.load import deserialize  # noqa: F401
 from safejax.core.save import serialize  # noqa: F401
