766c57897056352851458ad583c1014e02915850
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-12-31 14:33:14 +0100

    ✨ Add `fs` param

--- src/safejax/core/save.py
+++ src/safejax/core/save.py
@@ -1,6 +1,9 @@
+import os
+import tempfile
 from pathlib import Path
 from typing import Union
 
+from fsspec import AbstractFileSystem
 from safetensors.flax import save, save_file
 
 from safejax.typing import ParamsDictLike, PathLike
@@ -10,6 +13,7 @@ from safejax.utils import flatten_dict
 def serialize(
     params: ParamsDictLike,
     filename: Union[PathLike, None] = None,
+    fs: Union[AbstractFileSystem, None] = None,
 ) -> Union[bytes, PathLike]:
     """
     Serialize JAX, Flax, Haiku, or Objax model params from either `FrozenDict`, `Dict`, or `VarCollection`.
@@ -20,6 +24,7 @@ def serialize(
     Args:
         params: A `FrozenDict`, a `Dict` or a `VarCollection` containing the model params.
         filename: The path to the file where the model params will be saved.
+        fs: The filesystem to use to save the model params. Defaults to `None`.
 
     Returns:
         The serialized model params as a `bytes` object or the path to the file where the model params were saved.
@@ -32,10 +37,28 @@ def serialize(
                 "If `filename` is provided (not `None`), it must be a `str` or a"
                 f" `pathlib.Path` object, not {type(filename)}."
             )
-        filename = filename if isinstance(filename, Path) else Path(filename)
-        if not filename.exists or not filename.is_file:
-            raise ValueError(f"`filename` must be a valid file path, not {filename}.")
-        save_file(tensors=params, filename=filename.as_posix())
+        if fs and fs.protocol != "file":
+            if not isinstance(fs, AbstractFileSystem):
+                raise ValueError(
+                    "`fs` must be a `fsspec.AbstractFileSystem` object or `None`,"
+                    f" not {type(fs)}."
+                )
+            temp_filename = tempfile.NamedTemporaryFile(
+                mode="wb", suffix=".safetensors", delete=False
+            )
+            try:
+                temp_filename.write(save(tensors=params))
+            finally:
+                temp_filename.close()
+                fs.put_file(lpath=temp_filename.name, rpath=filename)
+                os.remove(temp_filename.name)
+        else:
+            filename = filename if isinstance(filename, Path) else Path(filename)
+            if not filename.exists or not filename.is_file:
+                raise ValueError(
+                    f"`filename` must be a valid file path, not {filename}."
+                )
+            save_file(tensors=params, filename=filename.as_posix())
         return filename
 
     return save(tensors=params)
