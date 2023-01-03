a5615c21d5f82a7b1c1a5e30c8cbeefee867f5dd
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-01-03 19:02:25 +0100

    🧪 Add `fs` fixture & add `_in_fs` unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import fsspec
 import haiku as hk
 import jax
 import jax.numpy as jnp
@@ -8,6 +9,7 @@ import pytest
 from flax import linen as nn
 from flax.core.frozen_dict import FrozenDict
 from flaxmodels.resnet import ResNet50 as FlaxResNet50
+from fsspec.spec import AbstractFileSystem
 from objax.variable import VarCollection
 from objax.zoo.resnet_v2 import ResNet50 as ObjaxResNet50
 
@@ -93,3 +95,8 @@ def safetensors_file(tmp_path_factory) -> Path:
 @pytest.fixture(scope="session")
 def msgpack_file(tmp_path_factory) -> Path:
     return Path(tmp_path_factory.mktemp("data") / "params.msgpack")
+
+
+@pytest.fixture
+def fs() -> AbstractFileSystem:
+    return fsspec.filesystem("file")
--- tests/test_core_load.py
+++ tests/test_core_load.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Union
 
 import pytest
 from flax.core.frozen_dict import FrozenDict
+from fsspec.spec import AbstractFileSystem
 from objax.variable import VarCollection
 
 from safejax.core.load import deserialize
@@ -76,6 +77,46 @@ def test_deserialize_from_file(
 ) -> None:
     safetensors_file = serialize(params=params, filename=safetensors_file)
     decoded_params = deserialize(path_or_buf=safetensors_file, **deserialize_kwargs)
+    assert isinstance(decoded_params, expected_output_type)
+    assert len(decoded_params) > 0
+    assert id(decoded_params) != id(params)
+    assert decoded_params.keys() == params.keys()
+
+
+@pytest.mark.parametrize(
+    "params, deserialize_kwargs, expected_output_type",
+    [
+        (
+            pytest.lazy_fixture("flax_resnet50_params"),
+            {"freeze_dict": True},
+            FrozenDict,
+        ),
+        (pytest.lazy_fixture("flax_resnet50_params"), {"freeze_dict": False}, dict),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": True},
+            VarCollection,
+        ),
+        (
+            pytest.lazy_fixture("objax_resnet50_params"),
+            {"requires_unflattening": False, "to_var_collection": False},
+            dict,
+        ),
+        (pytest.lazy_fixture("haiku_resnet50_params"), {}, dict),
+    ],
+)
+@pytest.mark.usefixtures("safetensors_file", "fs")
+def test_deserialize_from_file_in_fs(
+    params: FrozenDict,
+    deserialize_kwargs: Dict[str, Any],
+    expected_output_type: Union[dict, FrozenDict, VarCollection],
+    safetensors_file: Path,
+    fs: AbstractFileSystem,
+) -> None:
+    safetensors_file = serialize(params=params, filename=safetensors_file, fs=fs)
+    decoded_params = deserialize(
+        path_or_buf=safetensors_file, fs=fs, **deserialize_kwargs
+    )
     assert isinstance(decoded_params, expected_output_type)
     assert len(decoded_params) > 0
     assert id(decoded_params) != id(params)
