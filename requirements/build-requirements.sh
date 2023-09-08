c7e95bc1e6399a7b32dd2e5b8dedecba1f434b57
Ian Holmes <ihholmes@gmail.com>
2023-09-08 12:59:02 -0700

    fixed build-requirements script, used python 3.9

--- requirements/build-requirements.sh
+++ requirements/build-requirements.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 SCRIPT=$(readlink -f "$0")
-REQPATH=$(dirname $SCRIPT)
-ROOTPATH=$REQPATH/..
-pip-compile --output-file=$REQPATH/requirements.txt $ROOTPATH/pyproject.toml
-pip-compile --extra=test --output-file=$REQPATH/requirements-test.txt $ROOTPATH/pyproject.toml
-pip-compile --extra=docs --output-file=$REQPATH/requirements-docs.txt $ROOTPATH/pyproject.toml
-pip-compile --extra=quality --output-file=$REQPATH/requirements-dev.txt $ROOTPATH/pyproject.toml
+ROOTPATH=$(dirname $SCRIPT)/..
+cd $ROOTPATH
+pip-compile --output-file=requirements/requirements.txt pyproject.toml
+pip-compile --extra=test --output-file=requirements/requirements-test.txt pyproject.toml
+pip-compile --extra=docs --output-file=requirements/requirements-docs.txt pyproject.toml
+pip-compile --extra=quality --output-file=requirements/requirements-dev.txt pyproject.toml
